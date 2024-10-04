5035c5135cb819271acef52d5353d8405fa62f47
Code with the Flow <codewiththeflowxyz@proton.me>
2024-10-04 17:56:00 +0200

    Add csp in the middleware

---
+++ middleware.ts
@@ -0,0 +1,102 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const STYLE_HASHES = `
+'sha256-tTgjrFAQDNcRW/9ebtwfDewCTgZMFnKpGa9tcHFyvcs=' 
+'sha256-gG7n2yImybWYmjxjEMN7KEo3MhyMGgjqn8gRz+eG+ig=' 
+'sha256-DOBk7++YHUOjJNY/f4GsbHteEg5HdTneaiCEfy5uQM4=' 
+'sha256-bQ/N51E+fPi509ycrmiURBvjRnMixxLHsJTiWxmVnwI=' 
+'sha256-L2URSo6+HhBehpZl7MA4Q7bXinwzlyDCUtwJ7Grq5Qc=' 
+'sha256-zlqnbDt84zf1iSefLU/ImC54isoprH/MRiVZGskwexk=' 
+'sha256-FNzHEsO3ivNzNvaasOSqAP7ATlqepSOGMRsuwNSFmpg=' 
+'sha256-fGfB2ufs7RM3AYoBCxY6m7oXjj2A5OeCxKMWsgQU1K0=' 
+'sha256-Sx2rdNCa32VMPwGfowmkQ7OFdPMbXjQ5fxeS2KM/yYk=' 
+'sha256-mTXRdh19cZhwEoA6wECMOWIRS6jtk21+hCYrt3iWBIE=' 
+'sha256-/nS3Qf4aeagZUEddnV34TNpVs4fVSQi+7dsvzZt4N7k=' 
+'sha256-vjFrCzgXcGH5ZGzyVfSYn1+kvxqc6IYiuyP+tzBIgEU=' 
+'sha256-ZDrxqUOB4m/L0JWL/+gS52g1CRH0l/qwMhjTw5Z/Fsc=' 
+'sha256-Ot54TIZWGjnVTtWcnc431yE3L93p55MWeuSDrZRFgUY=' 
+'sha256-U9bJDah4g50oYiULPKDsIe2HehDMclZNbGyAci+B7tI=' 
+'sha256-a8arGiAkgtp4UVVjDYD0fgDcbTQ0lL8Q65pM8QfZT/w=' 
+'sha256-lC7dsYmXNMrsRyRKOvSVH2CuWxOleCx7DZf3wPLoAC8=' 
+'sha256-hlnVA3bAlca5EoLbbv9d7yXFH3dtAGmOKTXuQNh9ATY=' 
+'sha256-P8zyhPOogHstISSmUWzrkSykDo5VzlVyDj6K96EtCEI=' 
+'sha256-wE87CojqihFEk2fF9TvTSJOqXYMaes3Cr3X6R5mLinE=' 
+'sha256-xUUHdsqxY/8VMdVevQgW8yTzlmcFF3/YQTiKIA6tXuA=' 
+'sha256-3EP1piOo/O4YWqWO7mQYW6fCsMcX8uB/C/w3Cgomac4=' 
+'sha256-KaL6zRlehYeyBkDHHTGx0GZh7W/eSJbr5ejtzrhWrqw=' 
+'sha256-MtxTLcyxVEJFNLEIqbVTaqR4WWr0+lYSZ78AzGmNsuA=' 
+'sha256-PhrR5O1xWiklTp5YfH8xWeig83Y/rhbrdb5whLn1pSg=' 
+'sha256-udqn+IRm1CC9ivfJc0LiM4qvHxmmdHbm6N+wfEZh1Co=' 
+'sha256-EixVU4gdxzf2V08KlY0sp61zPsMRaC1fM5GCJZKn5uY=' 
+'sha256-pnRY5F6FZIpalkfOQdkJanjYrE/bdUdAaubkSqV90zs=' 
+'sha256-6ne/rcAsp2tsHyIziH2h0cK9UiE6ZwpezSOpSD8UKe0=' 
+'sha256-QIjW/+aUzfg58HcITJNHkkCTGmLovNUIQbL+Zq2TsIE=' 
+`;
+
+export async function middleware(request: NextRequest) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+
+  // TODO: do we need 'unsafe-eval'? check if is a requirement to run it locally or also in other environments
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' https://vercel.live/ https://vercel.com https://static.cloudflareinsights.com;
+    connect-src 'self' 
+      https://cloudflareinsights.com 
+      https://cloudflare-eth.com 
+      https://staging-api.sky.money 
+      https://api.sky.money 
+      https://api.ipify.org
+      https://vercel.live/
+      https://vercel.com
+      wss://ws-us3.pusher.com
+      *.pusher.com
+      *.pusherapp.com;
+    style-src 'self' ${STYLE_HASHES} https://vercel.live;
+    img-src 'self' data: blob: https://vercel.live https://vercel.com;
+    font-src 'self' https://vercel.live https://assets.vercel.com;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'self';
+    upgrade-insecure-requests;
+    frame-src 'self'
+      https://vercel.live/
+      https://vercel.com
+`;
+
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader.replace(/\s{2,}/g, ' ').trim();
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('x-nonce', nonce);
+
+  requestHeaders.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders
+    }
+  });
+  response.headers.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
+
+  return response;
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' }
+      ]
+    }
+  ]
+};
--- next.config.js
+++ next.config.js
@@ -1,49 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live/ https://vercel.com https://static.cloudflareinsights.com;
-    connect-src 'self' 
-      https://cloudflareinsights.com 
-      https://cloudflare-eth.com 
-      https://staging-api.sky.money 
-      https://api.sky.money 
-      https://api.ipify.org
-      https://vercel.live/
-      https://vercel.com
-      wss://ws-us3.pusher.com
-      *.pusher.com
-      *.pusherapp.com;
-    style-src 'self' https://vercel.live 'unsafe-inline';
-    img-src 'self' data: blob: https://vercel.live https://vercel.com;
-    font-src 'self' https://vercel.live https://assets.vercel.com;
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'self';
-    upgrade-insecure-requests;
-    frame-src 'self'
-      https://vercel.live/
-      https://vercel.com
-`;
-
 const nextConfig = {
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, '')
-          }
-        ]
-      }
-    ];
+  // Add this configuration to add nonce to the script tags
+  experimental: {
+    strictNextHead: true
   }
 };
 
