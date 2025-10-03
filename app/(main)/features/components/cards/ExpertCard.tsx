3e508be8d77da9a913d1ed443891979e54fa0893
Jetstream Dev <jetstreamgg@proton.me>
2025-10-03 15:43:35 -0600

    update features page card image

--- app/(main)/features/components/cards/ExpertCard.tsx
+++ app/(main)/features/components/cards/ExpertCard.tsx
@@ -1,16 +1,13 @@
 import Image from 'next/image';
-import { FeaturesPageCard, LiFeatureCard, TextFeatureCard } from '../FeaturesPageCard';
-import ProductsSkyBridge from '@/public/products_skybridge.png';
-import ProductsSkyBridgeMobile from '@/public/features-skylink-mobile.png';
+import { FeaturesPageCard, TextFeatureCard } from '../FeaturesPageCard';
+import ProductsStUSDS from '@/public/features_stUSDS_desktop.png';
+import ProductsStUSDSMobile from '@/public/features_stUSDS_mobile.png';
 import { useBreakpointIndex } from '@/app/hooks/useBreakpointIndex';
-import { useRandomL2Name } from '@/app/hooks/useRandomL2Name';
-import { ExternalLink } from '@/app/components/ExternalLink';
 import { FetchedData } from '@/app/(main)/fetchData';
 import { PopoverRateInfo } from '@/app/components/PopoverRateInfo';
 
 export const ExpertCard = ({ data }: { data: FetchedData }) => {
   const { bpi, isLoading: isLoadingBreakpointIndex } = useBreakpointIndex();
-  const randomL2Name = useRandomL2Name();
 
   return (
     <FeaturesPageCard
@@ -41,12 +38,18 @@ export const ExpertCard = ({ data }: { data: FetchedData }) => {
           url: `?widget=expert`,
           buttonVariant: 'azure-2',
           illustration: isLoadingBreakpointIndex ? null : (
-            <Image
-              alt="SkyBridge illustration"
-              src={bpi === 0 ? ProductsSkyBridgeMobile : ProductsSkyBridge}
-              className="pointer-events-none absolute top-1/2 w-full -translate-y-1/2 px-3 tablet:bottom-0 tablet:right-0 tablet:top-auto tablet:w-[160%] tablet:max-w-[160%] tablet:translate-x-8 tablet:translate-y-8 tablet:rounded-tl-[20px] tablet:px-0 desktop:translate-x-10 desktop:translate-y-10"
-              quality={100}
-            />
+            <div className="h-full w-full px-3 tablet:px-0">
+              <div className="relative flex h-full w-full flex-col">
+                <div className="grow" />
+                <Image
+                  alt="Stusds module"
+                  placeholder="blur"
+                  src={bpi === 0 ? ProductsStUSDSMobile : ProductsStUSDS}
+                  className="absolute top-1/2 h-auto w-full -translate-y-1/2 rounded-[20px] tablet:left-0 tablet:top-full tablet:translate-x-8 tablet:translate-y-[calc(-100%+92px)] tablet:rounded-r-none tablet:rounded-bl-none desktop:static desktop:top-auto desktop:translate-x-10 desktop:translate-y-10 desktop:rounded-tl-[40px]"
+                  quality={100}
+                />
+              </div>
+            </div>
           )
         }
       ]}
